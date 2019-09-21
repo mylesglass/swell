4b640fedee43ca004df239e6d8a6bdae0e4baa57
mylesglass <mylesalexanderglass@gmail.com>
2019-09-21 20:27:47 +1200

    NetlifyCMS added

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,6 +14,9 @@ export default {
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
       { rel: 'stylesheet', href: 'https://rsms.me/inter/inter-ui.css' }
+    ],
+    script: [
+      { src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }
     ]
   },
   /*
