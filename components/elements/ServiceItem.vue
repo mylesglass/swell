60a81151ab6c6b2bae84e9c3643ddf248deafe8f
mylesglass <mylesalexanderglass@gmail.com>
2019-10-21 11:10:47 +1300

    Update ServiceItem.vue

--- components/elements/ServiceItem.vue
+++ components/elements/ServiceItem.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="column is-4-desktop is-4-tablet is-6-mobile has-text-centered">
-    <figure class="image">
+    <figure class="image is-square is-service-image">
       <img :src="image">
     </figure>
     <p class="heading-2 has-text-white">
@@ -24,3 +24,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.is-service-image {
+  max-width: 4rem;
+  margin: auto;
+}
+</style>
